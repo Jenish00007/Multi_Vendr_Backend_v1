6b39d5c8e2c0b715ea7cdceefcacd22f9c902719
Jenish000007 <jenish.m0708@gmail.com>
2025-05-27 16:33:24 +0530

    backend fix

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -34,78 +34,18 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     const filename = req.file.filename;
     const fileUrl = path.join(filename);
 
-    const user = {
-      name: name,
-      email: email,
-      password: password,
+    const user = await User.create({
+      name,
+      email,
+      password,
       avatar: fileUrl,
-    };
+    });
 
-    const activationToken = createActivationToken(user);
-
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
-
-    // send email to user
-    try {
-      await sendMail({
-        email: user.email,
-        subject: "Activate your account",
-        message: `Hello  ${user.name}, please click on the link to activate your account ${activationUrl} `,
-      });
-      res.status(201).json({
-        success: true,
-        message: `please check your email:- ${user.email} to activate your account!`,
-      });
-    } catch (err) {
-      return next(new ErrorHandler(err.message, 500));
-    }
+    sendToken(user, 201, res);
   } catch (err) {
     return next(new ErrorHandler(err.message, 400));
   }
 });
-
-// create activation token
-const createActivationToken = (user) => {
-  // why use create activatetoken?
-  // to create a token for the user to activate their account  after they register
-  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
-    expiresIn: "5m",
-  });
-};
-
-// activate user account
-router.post(
-  "/activation",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const { activation_token } = req.body;
-
-      const newUser = jwt.verify(
-        activation_token,
-        process.env.ACTIVATION_SECRET
-      );
-      if (!newUser) {
-        return next(new ErrorHandler("Invalid token", 400));
-      }
-      const { name, email, password, avatar } = newUser;
-
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return next(new ErrorHandler("User already exists", 400));
-      }
-      user = await User.create({
-        name,
-        email,
-        avatar,
-        password,
-      });
-      sendToken(user, 201, res);
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
-);
 
 // login user
 router.post(
