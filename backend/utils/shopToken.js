b087a865bcad91951a3780637a7825f5534efa4e
Jenish000007 <jenish.m0708@gmail.com>
2025-05-31 17:05:04 +0530

    fav

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -1,16 +1,8 @@
-// create token and saving that in cookies
+// create token and send response
 const sendShopToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // Options for cookies
-  const options = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-  };
-
-  res.status(statusCode).cookie("seller_token", token, options).json({
+  res.status(statusCode).json({
     success: true,
     user,
     token,
