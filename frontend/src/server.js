8a385d99e4bc58576447225cea65d053d289c639
Jenish000007 <jenish.m0708@gmail.com>
2025-06-01 13:12:48 +0530

    build

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://api.qauds.in/api/v2";
+export const server = "https://api.smartkal.in/api/v2";
 
-export const backend_url = "https://api.qauds.in/api/v2";
+export const backend_url = "https://api.smartkal.in/api/v2";
