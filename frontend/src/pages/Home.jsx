e9d77294c1ed7015fbdbc6f0a5686806728ea7ce
Jenish000007 <jenish.m0708@gmail.com>
2025-06-04 13:57:56 +0530

    user Website changes

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection />
+            {/* Other home page components */}
+        </div>
+    );
+};
+
+export default Home; 
