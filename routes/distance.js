c6dae05e992c5a6d92856077df81f6d4de3f5946
Jenish000007 <jenish.m0708@gmail.com>
2025-06-11 10:20:49 +0530

    ghgh

--- routes/distance.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDistance } = require('../controller/distanceController');
-
-router.get('/distance-api', getDistance);
-
-module.exports = router; 
