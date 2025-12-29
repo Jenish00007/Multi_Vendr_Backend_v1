9b9a3149b6070bd6d413bbbefa4efc8795cbd87f
Jenish <116103065+Jenish00007@users.noreply.github.com>
2025-12-29 14:22:31 +0530

    changes 29/12

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,25 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
+// socket.io setup
+const { setIO } = require("./socket");
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  },
+});
+setIO(io);
+io.on("connection", (socket) => {
+  socket.on("join_order", (orderId) => {
+    if (orderId) socket.join(String(orderId));
+  });
+  socket.on("join_deliveryman", (deliverymanId) => {
+    if (deliverymanId) socket.join(`dm:${String(deliverymanId)}`);
+  });
+});
+
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
